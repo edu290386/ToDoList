243c7b46a48d2d4e8f4dbd573ca9321932319341
edu290386 <eduardo.torres@outlook.com>
2023-06-18 23:31:11 -0500

    delete and check task ended

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const containerTask = document.querySelector('.container-task');
 const buttonAddTask = document.querySelector('.button-add');
 const inputTask = document.querySelector('.input-task');
 const deleteButton = document.querySelector('.delete-button');
+const checkButton = document.querySelector('.check-button');
 
 
 const listTask = [];
@@ -20,35 +21,50 @@ buttonAddTask.onclick = () => {
 }
 
 const createNewTask = (text) => {
-    newTask = { task: text , status: 1, created_at: new Date() };
+    newTask = { task: text , status: false, created_at: new Date() };
     listTask.push(newTask);
     renderTask(listTask);
 }
 
+const deleteTask = (task) => {
+    listTask.splice(task,1);    
+    renderTask(listTask);
+}   
+
+const checkTask = (task) => {
+    (listTask[task].status) ? listTask[task].status = false : listTask[task].status = true;
+    renderTask(listTask)
+}
 
 
 const renderTask = (tasks) => {
     containerTask.innerHTML = "";
+    console.log(listTask);
     tasks.forEach((task, index) => { 
         containerTask.innerHTML += `
-        <div class="task ${index}">
-                    <div class="task-text ${index}">${task.task}
-                    </div>
-                    <div class="options ${index}">
-                        <button class="check-button ${index}" >
-                            <img src="./assets/icons/check-fill.png" alt="">
-                        </button>
-                        <button class="edit-button ${index}">
-                            <img src="./assets/icons/pencil-line.png" alt="">
-                        </button>
-                        <button class="save-button ${index}">
-                            <img src="./assets/icons/save-line.png" alt="">
-                        </button>
-                        <button class="delete-button ${index}">
-                            <img src="./assets/icons/delete-bin-line.png" alt="">
-                        </button>
-                    </div>
-                </div>`;
+        ${(task.status) ? `<div id="task-${index}" class="task done">`
+            : `<div id="task-${index}" class="task">`}
+            <div class="task-text">${task.task}
+            </div>
+            <div class="options ${index}">
+                <div>
+                    <button class="check-button" onclick=checkTask(${index})>
+                        <img src="./assets/icons/check-fill.png" alt="">
+                    </button>
+                    <button id="edit-button ${index}">
+                        <img src="./assets/icons/pencil-line.png" alt="">
+                    </button>
+                </div>
+                <div>
+                    <button id="save-button ${index}">
+                        <img src="./assets/icons/save-line.png" alt="">
+                    </button>
+                    <button class="delete-button" onclick=deleteTask(${index})>
+                        <img src="./assets/icons/delete-bin-line.png" alt="">
+                    </button>
+                </div>
+            </div>        
+        </div>`;
     });
 }
 
